9f27a15946cfc0e3e236e0f65a97283bcd3d61ea
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2020-06-15 09:14:18 +0200

    Initialize package

---
+++ src/Domain/ValueObject/ValueObject.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 15/06/2020
+ */
+
+declare(strict_types=1);
+
+/**
+ * Interface ValueObject
+ * @psalm-immutable
+ */
+interface ValueObject
+{
+
+}
