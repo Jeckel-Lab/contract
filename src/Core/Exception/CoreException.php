6f8e88479044a8bfc90b99969aa2856031c486fd
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2020-06-16 00:00:37 +0200

    Add command dispatcher exception

---
+++ src/Core/Exception/CoreException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 15/06/2020
+ */
+
+declare(strict_types=1);
+
+namespace JeckelLab\Contract\Core\Exception;
+
+use Throwable;
+
+/**
+ * Interface CoreException
+ * @package JeckelLab\Contract\Core\Exception
+ * @psalm-immutable
+ */
+interface CoreException extends Throwable
+{
+
+}
