2b1e7d5963d4a77f8188f075cb4e6cc138cae594
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2020-06-18 11:32:48 +0200

    Add domain event aware interface and trait

---
+++ src/Domain/Entity/DomainEventAwareTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 26/03/2020
+ */
+
+declare(strict_types=1);
+
+namespace JeckelLab\Contract\Domain\Entity;
+
+use JeckelLab\Contract\Domain\Event\Event;
+
+/**
+ * Trait DomainEventAwareEntityTrait
+ * @package JeckelLab\Contract\Domain\Entity
+ */
+trait DomainEventAwareTrait
+{
+    /** @var Event[] */
+    private $events = [];
+
+    /**
+     * @return Event[]
+     */
+    public function popEvents(): array
+    {
+        $events = $this->events;
+        $this->events = [];
+        return $events;
+    }
+
+    /**
+     * @param Event $event
+     * @return void
+     */
+    public function addDomainEvent(Event $event): void
+    {
+        $this->events[] = $event;
+    }
+}
