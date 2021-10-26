9b16181b1118b74c673071a764636fa57b8d90f7
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2021-10-26 13:02:20 +0200

    Add ViewModel interface

---
+++ src/Presentation/ViewModel/ViewModel.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 26/10/2021
+ */
+
+declare(strict_types=1);
+
+namespace JeckelLab\Contract\Presentation\ViewModel;
+
+use JsonSerializable;
+
+/**
+ * @psalm-immutable
+ */
+interface ViewModel extends JsonSerializable
+{
+
+}
