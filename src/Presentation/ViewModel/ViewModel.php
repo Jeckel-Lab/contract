de892082b0fc29d1d3b26da70d46ace28097a474
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2021-11-16 08:38:05 +0100

    Update QA tools

--- src/Presentation/ViewModel/ViewModel.php
+++ src/Presentation/ViewModel/ViewModel.php
@@ -9,12 +9,10 @@ declare(strict_types=1);
 
 namespace JeckelLab\Contract\Presentation\ViewModel;
 
-use JsonSerializable;
-
 /**
  * @psalm-immutable
  */
-interface ViewModel extends JsonSerializable
+interface ViewModel
 {
 
 }
