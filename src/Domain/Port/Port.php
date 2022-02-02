2d18c5cf9e40208b57f297b68d8da02ee4b63270
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2022-02-02 17:17:18 +0100

    Add Port interface

---
+++ src/Domain/Port/Port.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 02/02/2022
+ */
+
+namespace JeckelLab\Contract\Domain\Port;
+
+/**
+ * Interface Port
+ * @package JeckelLab\Contract\Domain\Port
+ */
+interface Port
+{
+}
